d5dd4f99d7b66ef9c702b5c052256a70a67e198d
vivek8271 <vivekkumar37904@gmail.com>
2026-05-11 07:06:30 +0000

    marquee

---
+++ java/dsa/slidingWindow.java
@@ -0,0 +1,26 @@
+class slidingWindow {
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,1,3,2};
+
+        int k = 3;  
+
+        int windowSum = 0;
+
+        for(int i=0; i<k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        for(int i=k; i<arr.length; i++) {
+
+            windowSum += arr[i];
+            windowSum -= arr[i-k];
+
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        System.out.println(maxSum); 
+
+    }
+}
---
+++ marquee/simple.java
@@ -0,0 +1,6 @@
+class simple{
+    public static void main(String args[]) {
+        String text = "Welcome to the Marquee!";
+        System.out.println(text);
+    }
+}
