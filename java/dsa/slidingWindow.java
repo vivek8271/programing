ce8d0f6f9c9eeb5ef5a276a6ab8fb37d5b23fc4b
vivek8271 <vivekkumar37904@gmail.com>
2026-05-12 02:18:15 +0000

    index page

--- java/dsa/slidingWindow.java
+++ java/dsa/slidingWindow.java
@@ -3,9 +3,7 @@ class slidingWindow {
         int[] arr = {2,1,5,1,3,2};
 
         int k = 3;  
-
         int windowSum = 0;
-
         for(int i=0; i<k; i++) {
             windowSum += arr[i];
         }
